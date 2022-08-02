034996a8c2d1da2428d2b2eab5cfbe5d305e8140
milol bayegui <clooney.lysandrepro@gmail.com>
2022-08-02 08:59:31 +0100

    this is the second commit

---
+++ mediaTECH/src/main/java/com/fstg/mediaTECH/MediaTechApplication.java
@@ -0,0 +1,13 @@
+package com.fstg.mediaTECH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaTechApplication.class, args);
+	}
+
+}
