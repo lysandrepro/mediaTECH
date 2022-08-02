b2ccf723fbd9b92f595e5d5eb25036acbc8d4846
milol bayegui <clooney.lysandrepro@gmail.com>
2022-08-02 09:35:12 +0100

    TO see if i've mastered everything

---
+++ proof_FRESHVOTE/src/main/java/com/fstg/proof_FRESHVOTE/ProofFreshvoteApplication.java
@@ -0,0 +1,13 @@
+package com.fstg.proof_FRESHVOTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofFreshvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProofFreshvoteApplication.class, args);
+	}
+
+}
